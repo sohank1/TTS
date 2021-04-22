0902e1dee1338322b2966b1f5a1b40bb10363d50
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-04-22 10:17:59 -0400

    Change WaitForWsAndAuth

--- src/modules/auth/WaitForWsAndAuth.tsx
+++ src/modules/auth/WaitForWsAndAuth.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useConn } from "../../shared-hooks/useConn";
+import { useHasTokens } from "./useHasTokens";
 
 export const WaitForWsAndAuth: React.FC = ({ children }) => {
     const conn = useConn();
 
+    // If user is not logged in just show the page
+    if (!useHasTokens()) return <>{children}</>;
+
+    // If user is logged in but user not loaded yet return nothing
     if (!conn?.user) return null;
 
+    // Once user is loaded show the page
     return <>{children}</>;
 }
