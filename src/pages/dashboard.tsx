1ff1ae44b4303cb8213d9dc8eafa9a898fe13358
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-04-22 10:32:16 -0400

    test

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 
 import { useContext, useEffect } from "react";
+import { useHasTokens } from "../modules/auth/useHasTokens";
 import { WaitForWsAndAuth } from "../modules/auth/WaitForWsAndAuth";
 import { WebSocketContext } from "../modules/ws/WebSocketProvider";
 import { useConn } from "../shared-hooks/useConn";
@@ -7,18 +8,22 @@ import { useConn } from "../shared-hooks/useConn";
 const DashboardPage = () => {
     // const { setConn } = useContext(WebSocketContext);
     const conn = useConn();
+    const hasTokens = useHasTokens();
     useEffect(() => {
         // setConn(null);
         console.log("conn", conn);
     }, [conn])
 
     return (
-        <WaitForWsAndAuth>
-            <p>
+        // <WaitForWsAndAuth>
+        <p>
+            {hasTokens ? <h3> You are not logged in</h3> : null}
+            {conn?.user ? <>
                 {JSON.stringify(conn?.user, null, 4)}
-                <img src={conn?.user?.avatarUrl}></img>
-            </p>
-        </WaitForWsAndAuth>
+                <img src={conn?.user?.avatarUrl} /></> : null}
+
+        </p>
+        // </WaitForWsAndAuth>
     )
 }
 
