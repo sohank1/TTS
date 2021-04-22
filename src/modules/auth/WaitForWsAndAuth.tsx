c82382610ebb104494e74d69a9b96a9ad593de09
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-04-22 10:08:01 -0400

    Update WaitForWsAndAuth.tsx

--- src/modules/auth/WaitForWsAndAuth.tsx
+++ src/modules/auth/WaitForWsAndAuth.tsx
@@ -4,7 +4,7 @@ import { useConn } from "../../shared-hooks/useConn";
 export const WaitForWsAndAuth: React.FC = ({ children }) => {
     const conn = useConn();
 
-    if (!conn?.user) return null;
+    if (!conn?.user) return <h2>loading...</h2>;
 
     return <>{children}</>;
 }
