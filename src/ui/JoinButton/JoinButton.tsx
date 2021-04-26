cf6e02d6ff820b10b1c114551791b14847ff4121
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-04-26 09:28:20 -0400

    JoinButton now opens in new tab

--- src/ui/JoinButton/JoinButton.tsx
+++ src/ui/JoinButton/JoinButton.tsx
@@ -4,6 +4,6 @@ export const inviteLink = 'https://discord.gg/RNTemwY';
 
 export const JoinButton = () => (
     <div className="join-button-ui-component">
-        <Button variant="contained" color="primary" href={inviteLink}>Join</Button>
+        <Button variant="contained" color="primary" target="_blank" href={inviteLink}>Join</Button>
     </div>
 )
