5be98a5f784b82b100cf032346ebcb86daa5c90c
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-05-04 08:25:08 -0400

    server down in working good state

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,27 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head></Head>
+                <Head>
+                    <title>TTS</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0" />
+                    <meta property="og:locale" content="en-US" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://ttsclan.vercel.app/" />
+                    <meta property="og:site_name" content="TTS" />
+                    <meta property="og:keywords"
+                        content="TTS,Dashboard,Fortnite Clan,TTS Clan,Tomatohead,tomatohead society,tomatohead clan" />
+                    <meta property="og:title" content="TTS" />
+                    <meta property="og:description" content="The Tomatohead Society (TTS) is a Fortnite clan that strongly supports Team Pizza and is always looking for new members like you!" />
+                    <meta property="og:image" content="https://ttsclan.vercel.app/assets/preview.png" />
+                    <meta name="theme-color" content="#ff3f3f" />
+
+                    <meta property="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:site" content="@TheTomatoHeads" />
+                    <meta property="twitter:site:id" content="@TheTomatoHeads" />
+                    <meta property="twitter:creator" content="@TheTomatoHeads" />
+                    <meta property="twitter:creator:id" content="@TheTomatoHeads" />
+                    <meta property="twitter:image" content="https://ttsclan.vercel.app/assets/preview.png" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
--- src/ui/ServerDownModal/ServerDownModal.tsx
+++ src/ui/ServerDownModal/ServerDownModal.tsx
@@ -8,15 +8,13 @@ export const ServerDownModal = () => {
         <>
             {isOpen ?
                 <div className="server-down-modal-ui-component">
-                    <div className="modal">
-                        <section>
-                            <Button onClick={() => setIsOpen(false)} id="close">
-                                <img src="assets/close.svg" />
-                            </Button>
-                        </section>
-                        <h1>Failed to Establish Connection to Websocket Server</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae odio, illum aliquam quos expedita, consectetur quisquam hic quam corrupti ipsam nulla ducimus? Tempore molestias obcaecati repudiandae nesciunt vel error dolore!</p>
-                    </div>
+                    <section>
+                        <Button onClick={() => setIsOpen(false)} id="close">
+                            <img src="assets/close.svg" />
+                        </Button>
+                    </section>
+                    <h1>Failed to Establish Connection to Websocket Server</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae odio, illum aliquam quos expedita, consectetur quisquam hic quam corrupti ipsam nulla ducimus? Tempore molestias obcaecati repudiandae nesciunt vel error dolore!</p>
                 </div>
                 : null}
         </>
