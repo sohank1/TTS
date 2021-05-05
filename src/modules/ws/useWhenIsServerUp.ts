41badd5f3b7d2a1850711437bc1c8a627cd2c09c
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-05-05 11:31:42 -0400

    useWhenIsServerUp hook

---
+++ src/modules/ws/useWhenIsServerUp.ts
@@ -0,0 +1,17 @@
+/**
+ * Calculates when the Websocket Server will be back up
+ * Date is automatically converted to client's local time
+ */
+export const useWhenIsServerUp = (): string => {
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+
+    const d = new Date();
+    d.setUTCSeconds(0);
+    d.setUTCMinutes(0);
+    d.setUTCHours(0);
+    d.setUTCMonth(month, 1);
+    d.setUTCFullYear(year);
+
+    return d.toLocaleDateString();
+}
--- src/ui/ServerDownModal/ServerDownModal.tsx
+++ src/ui/ServerDownModal/ServerDownModal.tsx
@@ -1,10 +1,12 @@
 import { useState, useRef, useEffect } from "react";
 import { Button, ClickAwayListener } from "@material-ui/core";
 import { CloseIcon } from "../../icons/CloseIcon";
+import { useWhenIsServerUp } from "../../modules/ws/useWhenIsServerUp";
 
 export const ServerDownModal = () => {
     const [isOpen, setIsOpen] = useState(true);
     const modal = useRef<HTMLDivElement>(null);
+    const serverUpAt = useWhenIsServerUp();
 
     useEffect(() => {
         document.body.style.overflow = "hidden";
@@ -33,7 +35,7 @@ export const ServerDownModal = () => {
 
                     <div className="text">
                         <h1>Failed to Establish Connection to Websocket Server</h1>
-                        <p>The Websocket Server is currently down. Because of this, most of the website will no longer function properly. Please return at (5/1/21 00:00 UTC) to properly use the website.</p>
+                        <p>The Websocket Server is currently down. Because of this, most of the website will no longer function properly. Please return at {serverUpAt} to properly use the website.</p>
                         <div className="understand">
                             <Button onClick={close} >I Understand</Button>
                         </div>
