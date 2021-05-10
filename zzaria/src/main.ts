3d7e07d5b6142a4c37a279e8d4d6cad4b027ff4c
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-05-10 14:47:14 -0400

    refactor(zzaria): rm old session stuff

--- zzaria/src/main.ts
+++ zzaria/src/main.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import * as session from "express-session";
-import * as passport from "passport";
-import * as cookieParser from "cookie-parser";
-import * as fileUpload from "express-fileupload";
+// import * as session from "express-session";
+// import * as passport from "passport";
+// import * as cookieParser from "cookie-parser";
+// import * as fileUpload from "express-fileupload";
 import { environment } from "./environment/environment";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { NotFoundExceptionFilter } from "./not-found-exception.filter";
@@ -22,21 +22,21 @@ async function bootstrap(): Promise<void> {
         credentials: true,
     });
 
-    app.use(cookieParser());
-    app.use(
-        session({
-            secret: "Testing",
-            cookie: {
-                maxAge: 60 * 1000 * 60 * 24,
-            },
-            resave: false,
-            saveUninitialized: false,
-        })
-    );
+    // app.use(cookieParser());
+    // app.use(
+    //     session({
+    //         secret: "Testing",
+    //         cookie: {
+    //             maxAge: 60 * 1000 * 60 * 24,
+    //         },
+    //         resave: false,
+    //         saveUninitialized: false,
+    //     })
+    // );
 
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(fileUpload());
+    // app.use(passport.initialize());
+    // app.use(passport.session());
+    // app.use(fileUpload());
 
     await app.listen(environment.PORT);
 }
