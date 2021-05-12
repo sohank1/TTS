2c49814e44e0a7e7d5da621c511667e72b86b469
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-05-12 18:05:47 -0400

    fix(zzaria): do not refresh refresh token

--- zzaria/src/auth/auth.service.ts
+++ zzaria/src/auth/auth.service.ts
@@ -148,7 +148,7 @@ export class AuthService {
                     userId: r.userId,
                     tokens: {
                         accessToken: this._signAccessToken(r.userId),
-                        refreshToken: this._signRefreshToken(r.userId),
+                        refreshToken: r
                     },
                 });
             }
