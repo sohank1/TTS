fd1954bfe9df0c6dd6228295a3b0421866fbc340
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-05-29 17:35:16 -0400

    Update updateVersion.js

--- axeroni/scripts/updateVersion.js
+++ axeroni/scripts/updateVersion.js
@@ -3,3 +3,5 @@ const pkj = require("../package.json");
 
 pkj.version = `${(Number(pkj.version[0]) + 1).toString()}.0.0`;
 fs.writeFile("./package.json", JSON.stringify(pkj, null, 2));
+
+console.log(`Updated Axeroni Version: ${pkj.version}`);
