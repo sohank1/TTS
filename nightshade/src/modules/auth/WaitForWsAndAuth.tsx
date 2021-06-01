1c58923512e0bda3c628c1904b7bf5e19d274e73
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-06-01 11:56:19 -0400

    Update WaitForWsAndAuth.tsx

--- nightshade/src/modules/auth/WaitForWsAndAuth.tsx
+++ nightshade/src/modules/auth/WaitForWsAndAuth.tsx
@@ -9,7 +9,7 @@ export const WaitForWsAndAuth: React.FC = ({ children }) => {
     if (!conn) return <h1>loading</h1>;
 
     // If user is not logged in just show the page
-    if (!hasTokens) return <>{children}</>;
+    if (!hasTokens) return null;
 
     // If user is logged in but user not loaded yet return nothing
     if (!conn?.user) return null;
