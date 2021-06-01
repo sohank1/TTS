54026408756af4be667908e07f2ecfa266d15908
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-06-01 12:51:13 -0400

    Update connect.ts

--- crustina/src/connect.ts
+++ crustina/src/connect.ts
@@ -13,7 +13,7 @@ export const connect = (
         onClearTokens = () => {},
         url = apiUrl,
         getAuthOptions,
-        onUser,
+        onUser = () => {},
         onNewTokens,
     }: {
         onConnectionFailed?: () => void;
