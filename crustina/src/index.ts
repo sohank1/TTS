d99668f45cb810883c7e101dac3c452f60d1a02f
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-06-04 08:00:29 -0400

    Update index.ts

--- crustina/src/index.ts
+++ crustina/src/index.ts
@@ -2,4 +2,4 @@ export * from "./connect";
 export * from "./Connection";
 export * from "./Query";
 export * from "./types";
-export const VERSION_TEST = "TESTAFTERCOMMIT v12";
+export const VERSION_TEST = "TESTAFTERCOMMIT v11";
