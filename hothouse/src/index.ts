85ecf07b5190267721d74e0d62a357b4471eaa62
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-06-04 10:37:12 -0400

    test

--- hothouse/src/index.ts
+++ hothouse/src/index.ts
@@ -7,34 +7,32 @@ import { BaseCommand } from "./commands/baseCommand/BaseCommand";
 import { BaseEvent } from "./events/baseEvent/BaseEvent";
 import { BaseWsEvent } from "./ws/events/BaseWsEvent";
 
-(async function () {
-    client.on("ready", () => {
-        BaseCommand.register(join(__dirname, "commands"));
-        BaseEvent.register(join(__dirname, "events"));
-        BaseWsEvent.register(join(__dirname, "ws/events"));
+client.on("ready", () => {
+    BaseCommand.register(join(__dirname, "commands"));
+    BaseEvent.register(join(__dirname, "events"));
+    BaseWsEvent.register(join(__dirname, "ws/events"));
 
-        client.api.applications(client.user.id).commands.post({
-            data: {
-                name: "ping",
-                description: "Gets your ping to TTS Bot",
-            },
-        });
-
-        client.ws.on("INTERACTION_CREATE", async (interaction) => {
-            // Do stuff here - interaction is an InteractionResponse object. To get the name, use interaction.data.name
-            // In particular, the values you passed to the interaction when creating it will be passed back here
-
-            if (interaction.data.name === "ping") {
-                client.api.interactions(interaction.id, interaction.token).callback.post({
-                    data: {
-                        type: 4,
-                        data: {
-                            content: `Your ping is ${client.ws.ping}ms`,
-                            allowed_mentions: { parse: [] },
-                        },
-                    },
-                });
-            }
-        });
+    client.api.applications(client.user.id).commands.post({
+        data: {
+            name: "ping",
+            description: "Gets your ping to TTS Bot",
+        },
     });
-})();
+
+    client.ws.on("INTERACTION_CREATE", async (interaction) => {
+        // Do stuff here - interaction is an InteractionResponse object. To get the name, use interaction.data.name
+        // In particular, the values you passed to the interaction when creating it will be passed back here
+
+        if (interaction.data.name === "ping") {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: `Your ping is ${client.ws.ping}ms`,
+                        allowed_mentions: { parse: [] },
+                    },
+                },
+            });
+        }
+    });
+});
