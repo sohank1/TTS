bd4a895255d382b1544664de972bdfff01e9c22c
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-06-04 07:57:19 -0400

    Update updateVersion.js

--- crustina/scripts/updateVersion.js
+++ crustina/scripts/updateVersion.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises;
 const pkj = require("../package.json");
 
-pkj.version = `${(Number(pkj.version[0]) + 1).toString()}.0.0`;
+pkj.version = `${(Number(pkj.version.split(".")[0]) + 1).toString()}.0.0`;
 fs.writeFile("./package.json", JSON.stringify(pkj, null, 2));
 
 console.log(`Updated Crustina Version: ${pkj.version}`);
