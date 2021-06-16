b2f7aaebd1fdd01eab29ecab21449e78ac18edb2
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-06-16 09:47:56 -0400

    feat(nightshade): finish search bar + change layout

--- nightshade/src/modules/patch-notes/PatchNotesPage.tsx
+++ nightshade/src/modules/patch-notes/PatchNotesPage.tsx
@@ -2,6 +2,35 @@ import Link from "next/link";
 import { Button } from "@material-ui/core";
 import { DashboardNavBar } from "../../ui/DashboardNavBar";
 
+const data = [
+    {
+        name: "Penguin's Zone/Scrim Wars V2.1",
+        image: "https://i.imgur.com/OKIwF04.png",
+        author: {
+            name: "Mysterious_Penguin",
+            avatarUrl:
+                "https://cdn.discordapp.com/avatars/539928835953524757/fa626df27e371d63c56ee3f4ad809e4c.png?size=2048",
+        },
+        date: "1/20/19",
+        likes: 1,
+        dislikes: 2,
+        comments: 1,
+    },
+    {
+        name: "Penguin's Zone/Scrim Wars V2.1",
+        image: "https://i.imgur.com/OKIwF04.png",
+        author: {
+            name: "Mysterious_Penguin",
+            avatarUrl:
+                "https://cdn.discordapp.com/avatars/539928835953524757/fa626df27e371d63c56ee3f4ad809e4c.png?size=2048",
+        },
+        date: "1/20/19",
+        likes: 1,
+        dislikes: 2,
+        comments: 1,
+    },
+];
+
 export const PatchNotesPage = () => {
     return (
         <DashboardNavBar>
@@ -21,8 +50,12 @@ export const PatchNotesPage = () => {
                 <section className="panel">
                     <table>
                         <div className="search">
-                            <input type="text" placeholder="Search" />
-                            <img src="assets/search.svg" />
+                            <div className="container">
+                                <input type="text" placeholder="Search" />
+                                <button>
+                                    <img src="assets/search.svg" />
+                                </button>
+                            </div>
                         </div>
                         <tr className="top-row">
                             <th>Image</th>
@@ -30,33 +63,39 @@ export const PatchNotesPage = () => {
                             <th>Date</th>
                             {/* <th>Opinion</th> */}
                         </tr>
-                        <tr>
-                            <th className="thumbnail">
-                                <img src="https://i.imgur.com/OKIwF04.png" />
-                            </th>
-                            <th className="info">
-                                <div className="details">
-                                    <h1>Penguin's Zone/Scrim Wars V2.1</h1>
-                                    <div className="user">
-                                        <img
-                                            className="avatar"
-                                            src="https://cdn.discordapp.com/avatars/539928835953524757/fa626df27e371d63c56ee3f4ad809e4c.png?size=2048"
-                                        />
-                                        <h2>Mysterious_Penguin</h2>
+
+                        {data.map((p) => (
+                            <Link href="/">
+                                <a>
+                                    <tr>
+                                        <th className="thumbnail">
+                                            <img src={p.image} />
+                                        </th>
+
+                                        <th className="info">
+                                            <div className="details">
+                                                <h1>{p.name}</h1>
+                                                <div className="user">
+                                                    <img className="avatar" src={p.author.avatarUrl} />
+                                                    <h2>{p.author.name}</h2>
+                                                </div>
+                                            </div>
+                                        </th>
+
+                                        <th>
+                                            <h4 className="date">{p.date}</h4>
+                                        </th>
+                                    </tr>
+
+                                    <div className="body">
+                                        <h4 className="date">{p.date}</h4>
+                                        <h4 className="date">{p.likes}</h4>
+                                        <h4 className="date">{p.dislikes}</h4>
+                                        <h4 className="date">{p.comments}</h4>
                                     </div>
-                                </div>
-                            </th>
-
-                            <th>
-                                <h4 className="date">1/20/19</h4>
-                            </th>
-                        </tr>
-
-                        <div className="body">
-                            <h4 className="date">1/20/19</h4>
-                            <h4 className="date">1/20/19</h4>
-                            <h4 className="date">1/20/19</h4>
-                        </div>
+                                </a>
+                            </Link>
+                        ))}
                     </table>
                 </section>
             </div>
